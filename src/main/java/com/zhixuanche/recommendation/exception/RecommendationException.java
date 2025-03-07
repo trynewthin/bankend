3b760faeedcf7f522363c2b4cf4c36f8c9705d09
周心怡 <2453803178@qq.com>
2025-03-07 21:43:26 +0800

    控制层设计

--- src/main/java/com/zhixuanche/recommendation/exception/RecommendationException.java
+++ src/main/java/com/zhixuanche/recommendation/exception/RecommendationException.java
@@ -1,1 +1,25 @@
- 
+package com.zhixuanche.recommendation.exception;
+
+import com.zhixuanche.common.exception.ErrorCode;
+
+/**
+ * 推荐模块异常
+ */
+public class RecommendationException extends RuntimeException {
+    
+    private final int code;
+    
+    public RecommendationException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+    
+    public RecommendationException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+    
+    public int getCode() {
+        return code;
+    }
+} 
